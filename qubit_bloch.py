d1d06a9a84088e7bd37d56645887afd1541c9d8d
Osaid <80525901+R-20cF@users.noreply.github.com>
2026-06-24 19:09:40 +0500

    Initialize Git repository

---
+++ qubit_bloch.py
@@ -0,0 +1,40 @@
+from qiskit.visualization import plot_bloch_vector
+import numpy as np
+import sys
+
+# Get user input for qubit state
+while True:
+    # Ensure that user enters numbers
+    try:
+        alpha = int(input("Probability amplitude of state |0>: "))
+        beta = int(input("Probability amplitude of state |1>: "))
+    except ValueError:
+        print("Try again")
+
+    # Confirm that state is normalized
+    if abs(((abs(alpha) ** 2) + (abs(beta) ** 2)) - 1) > 0.0001:
+        print("State not normalized. Try again")
+    else:
+        break
+
+# Find spherical coordinates from amplitudes
+theta = np.arccos(alpha) * 2
+
+if int(theta) == 0:
+    phi = 0
+else:
+    exp_i_phi = beta/np.sin(theta/2)
+    phi = np.arccos(exp_i_phi.real)    
+
+# Specify state in spherical coords
+state = [1, theta, phi]
+
+# Plot state as Bloch sphere and save output
+plot_bloch_vector(state, title=f"Qubit with state {state}", coord_type="spherical").savefig("qubtit_state.png", bbox_inches="tight")
+
+
+
+
+
+
+
